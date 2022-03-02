66efd452c6d5bd98b6ebdc602d9f19c15ade1307
Bartek Bartosik <b.bartek987@gmail.com>
2022-03-02 22:22:01 +0000

    2nd commit

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// document.querySelectorAll('.story-btn').forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         btn.classList.toggle('change')
+//         btn.nextElementSibling.classList.toggle('change')
+//     })
+// })
+
+// window.onload = () => {
+//     setTimeout(() => {
+//       document.querySelector("body").classList.add("display");
+//     }, 4000);
+//   };
+  
+//   document.querySelector(".hamburger-menu").addEventListener("click", () => {
+//     document.querySelector(".container").classList.toggle("change");
+//   });
+  
+//   document.querySelector(".scroll-btn").addEventListener("click", () => {
+//     document.querySelector("html").style.scrollBehavior = "smooth";
+//     setTimeout(() => {
+//       document.querySelector("html").style.scrollBehavior = "unset";
+//     }, 1000);
+//   });
+  
